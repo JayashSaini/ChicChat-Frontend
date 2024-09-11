00ee2c0b3d4dd1efcb5cfb57c99b662406d69c68
Jayash Saini <jayashysaini7361@gmail.com>
2024-09-12 00:12:53 +0530

    refactor(var): remove unnecessary import

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+  static getDerivedStateFromError(): ErrorBoundaryState {
     // Update state so the next render will show the fallback UI
     return { hasError: true };
   }
